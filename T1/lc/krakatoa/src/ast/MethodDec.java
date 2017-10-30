ad1cdfde324456b3e0e89e9d5ec3a4a435344786
brunodonatob <bruno.donato.banhos@gmail.com>
2017-10-30 01:43:37 -0200

    MethodDec.java método addStatement implementado.

--- T1/lc/krakatoa/src/ast/MethodDec.java
+++ T1/lc/krakatoa/src/ast/MethodDec.java
@@ -1,5 +1,7 @@
 package ast;
 
+import java.util.ArrayList;
+
 import lexer.Symbol;
 
 public class MethodDec {
@@ -15,14 +17,20 @@ public class MethodDec {
 	public Symbol getQualifier() {
 		return qualifier;
 	}
+	
+	public void addStatement(Statement statement) {
+		statementList.add(statement);
+	}
 
 	public MethodDec(String name, Type returnType, Symbol qualifier) {
 		this.name = name;
 		this.returnType = returnType;
 		this.qualifier = qualifier;
+		this.statementList = new ArrayList<>();
 	}
 	
 	private String name;
 	private Type returnType;
 	private Symbol qualifier;
+	private ArrayList<Statement> statementList;
 }

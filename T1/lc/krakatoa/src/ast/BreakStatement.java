000dcedbbfbe6e5bf65c4943b929eaeaaddbdb0a
brunodonatob <bruno.donato.banhos@gmail.com>
2017-10-30 23:38:55 -0200

    Alteração no .gitignore
    Agora não considera os outputs nos commits.

---
+++ T1/lc/krakatoa/src/ast/BreakStatement.java
@@ -0,0 +1,17 @@
+package ast;
+
+public class BreakStatement extends Statement {
+
+	@Override
+	public void genC(PW pw) {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public void genKra(PW pw) {
+		pw.printIdent("break;");
+		
+	}
+
+}

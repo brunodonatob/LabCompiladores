51a5751461965b663ff393bfbfbc4326ededa35a
brunodonatob <bruno.donato.banhos@gmail.com>
2017-10-31 00:55:10 -0200

    Implementação do genKra() de InstanceVariable.java

--- T1/lc/krakatoa/src/ast/InstanceVariable.java
+++ T1/lc/krakatoa/src/ast/InstanceVariable.java
@@ -5,5 +5,11 @@ public class InstanceVariable extends Variable {
     public InstanceVariable( String name, Type type ) {
         super(name, type);
     }
+    
+    @Override
+    public void genKra(PW pw) {
+    	pw.printIdent("private "+ super.getType().toString() +" "+ super.getName()+";");
+    	pw.println("");
+    }
 
 }

348351305653e11fc7185454ced183f565f5d2f4
brunodonatob <bruno.donato.banhos@gmail.com>
2017-10-31 13:03:36 -0200

    Criação da classe CompositeStatement.java

---
+++ T1/lc/krakatoa/src/ast/CompositeStatement.java
@@ -0,0 +1,24 @@
+package ast;
+
+import java.util.ArrayList;
+
+public class CompositeStatement extends Statement {
+
+	public CompositeStatement(ArrayList<Statement> statementList) {
+		this.statementList = statementList;
+	}
+
+	@Override
+	public void genC(PW pw) {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public void genKra(PW pw) {
+		// TODO Auto-generated method stub
+		
+	}
+
+	private ArrayList<Statement> statementList;
+}

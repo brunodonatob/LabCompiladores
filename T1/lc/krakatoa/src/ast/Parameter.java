bc55cc181e2ab17007d588ce81a025756e2135ea
brunodonatob <bruno.donato.banhos@gmail.com>
2017-10-31 01:01:12 -0200

    Implementação do genKra() de Parameter.java

--- T1/lc/krakatoa/src/ast/Parameter.java
+++ T1/lc/krakatoa/src/ast/Parameter.java
@@ -6,5 +6,18 @@ public class Parameter extends Variable {
     public Parameter( String name, Type type ) {
         super(name, type);
     }
+    
+    @Override
+    public void genKra(PW pw) {
+    	pw.print(super.getType().toString());
+    	pw.print(" ");
+    	pw.print(super.getName());
+    }
+    
+    @Override
+    public void genC(PW pw) {
+    	
+    }
+
 
 }

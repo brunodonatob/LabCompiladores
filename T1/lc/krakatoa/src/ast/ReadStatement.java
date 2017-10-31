367c22fc32c5f6282e7433c40c660626b40b5fca
brunodonatob <bruno.donato.banhos@gmail.com>
2017-10-31 10:55:07 -0200

    Criação da classe ReadStatement.java

---
+++ T1/lc/krakatoa/src/ast/ReadStatement.java
@@ -0,0 +1,36 @@
+package ast;
+
+import java.util.*;
+
+public class ReadStatement extends Statement {
+
+	public ReadStatement(ArrayList<Variable> idList) {
+		this.idList = idList;
+	}
+	
+	@Override
+	public void genC(PW pw) {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public void genKra(PW pw) {
+		Iterator<Variable> varList = idList.iterator();
+		
+		pw.printIdent("read(");
+		
+		Variable var = varList.next();
+		var.genKra(pw);
+		
+		while(varList.hasNext()) {
+			pw.print(", ");
+			var = varList.next();
+			var.genKra(pw);
+		}
+		
+		pw.println(");");
+	}
+
+	ArrayList<Variable> idList;
+}

0eae2fcd1de319f0a97ac86117d6e30d9b00f27b
brunodonatob <bruno.donato.banhos@gmail.com>
2017-10-31 10:55:43 -0200

    Criação da classe WritelnStatement.java

---
+++ T1/lc/krakatoa/src/ast/WritelnStatement.java
@@ -0,0 +1,20 @@
+package ast;
+
+public class WritelnStatement extends WriteStatement {
+
+	public WritelnStatement(ExprList exprList) {
+		super(exprList);
+	}
+
+	@Override
+	public void genC(PW pw) {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public void genKra(PW pw) {
+		// TODO Auto-generated method stub
+		
+	}
+}

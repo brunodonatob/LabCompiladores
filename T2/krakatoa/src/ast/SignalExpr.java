2c1b9cddafea36b600f909a5bb4cce43168eb34c
brunodonatob <bruno.donato.banhos@gmail.com>
2017-12-07 11:04:23 -0200

    genCpp: SignalExpr

--- T2/krakatoa/src/ast/SignalExpr.java
+++ T2/krakatoa/src/ast/SignalExpr.java
@@ -17,7 +17,12 @@ public class SignalExpr extends Expr {
 
     @Override
 	public void genCpp( PW pw, boolean putParenthesis ) {
-  
+    	if ( putParenthesis )
+            pw.print("(");
+         pw.print( oper == Symbol.PLUS ? "+" : "-" );
+         expr.genCpp(pw, true);
+         if ( putParenthesis )
+            pw.print(")");
     }
     
     public void genKra(PW pw,boolean putParenthesis) {
